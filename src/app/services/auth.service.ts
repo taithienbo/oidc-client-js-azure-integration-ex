c67713dd8c0ea23c72a263f634b27136dfede6d7
Tai Bo <taithienbo@gmail.com>
2020-02-10 23:07:06 -0800

    adding authentication service.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { UserManager, User } from 'oidc-client';
+import { environment } from '../../environments/environment'; 
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  _userManager: UserManager; 
+
+  constructor() { }
+
+  public async loginRedirect(): Promise<any> {
+      return await this._userManager.signinRedirect(); 
+  }
+
+  public async loginSilent(): Promise<User> {
+    var user = await this._userManager.signinSilent(); 
+    return user; 
+  }
+
+  public async logout() {
+    await this._userManager.revokeAccessToken();
+    await this._userManager.clearStaleState();
+    await this._userManager.removeUser(); 
+}
+
+  public async logoutRedirect(): Promise<any> {
+      await this._userManager.signoutRedirect();
+      await this._userManager.clearStaleState(); 
+  }
+
+  public addUserUnloadedCallback(callback): void {
+      this._userManager.events.addUserUnloaded(callback);
+  }
+
+  public removeUserUnloadedCallback(callback): void {
+      this._userManager.events.removeUserLoaded(callback);
+  }
+
+  public addUserLoadedCallback(callback): void {
+      this._userManager.events.addUserLoaded(callback);
+  }
+
+  public removeUserLoadedCallback(callback): void {
+      this._userManager.events.removeUserLoaded(callback);
+  }
+
+  public async accessToken(): Promise<string> {
+      var user = await this._userManager.getUser();
+      if (user == null) {
+          throw new Error("User is not logged in");
+      }
+      return user.access_token;
+  }
+
+
+  public async getUser(): Promise<User> {
+      return this._userManager.getUser();
+  }
+
+
+  public instantiate() {
+      var settings = environment.oidcSettings;
+      this._userManager = new UserManager(settings);
+  }
+}
